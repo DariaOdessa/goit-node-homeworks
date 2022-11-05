b0a9110aa81964a264ee0fb222b480f98bfd9f94
DariaOdessa <dariaonlynew@gmail.com>
2022-11-05 22:21:30 +0200

    add HW-03

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,9 +1,9 @@
-const contactsOperations = require("../../models/contacts");
+const { Contact } = require("../../models");
 const { NotFound } = require("http-errors");
 
 const removeContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contactsOperations.removeContact(id);
+  const result = await Contact.findByIdAndDelete(id);
   if (!result) {
     throw new NotFound(`Contact with id=${id} not found!`);
   }
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+async function main() {
+  try {
+    if (!DB_HOST) {
+      throw new Error("DB_HOST not set!");
+    }
+
+    await mongoose.connect(DB_HOST);
+    console.log("Database connection successful");
+
+    app.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`server is listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+}
+main();
